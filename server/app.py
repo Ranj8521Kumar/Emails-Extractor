ff4de9783b68daa1f6227901b656875e7100c221
Pandit <22it3037@rgipt.ac.in>
2025-05-11 07:40:23 +0530

    Added blueprint and else for deployment at render

--- server/app.py
+++ server/app.py
@@ -62,5 +62,7 @@ def serve_client_index():
     return send_from_directory(client_dir, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use environment variable for port if available (for Render deployment)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
